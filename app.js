1dfa52b3a2358b7f4a02f1aeaebbdb147219b394
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2023-09-29 20:13:38 +0530

    feat added env and connected to db

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB (make sure you have MongoDB installed and running)
-mongoose.connect('mongodb://localhost/your-database-name', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 // Middleware
 app.use(express.json());
-
-// Include routes
-const apiRoutes = require('./routes');
-app.use('/', apiRoutes);
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString);
+const database = mongoose.connection
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
