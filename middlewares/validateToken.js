81232a741d81359a46d41ce700b6ea290ad9470b
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2023-10-24 18:51:02 +0530

    fix: token validate

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken')
 const secretKey = process.env.JWT_SECRET_KEY
 const validateToken = (req, res, next) => {
-  const token = req.headers.authorization
+  const authHeader = req.headers.authorization
+  const token = authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: 'Token is missing' })
