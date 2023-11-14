53655df150ddcb11bfe22030114f4fc6c2a2f141
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2023-11-14 22:15:55 +0530

    feat:added redux toolkit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port : 3000,
+  server: { // Serve the React app on port 8000
     proxy: {
-        '/': {
-            target: process.env.PROXY_TARGET||'http://localhost:5000',
-            changeOrigin: true,
-            secure: false
-        }
-    }
-
-},
+      '/api': {
+        target: 'http://127.0.0.1:3000/', // Redirect all /api requests to localhost:3000
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix from the request path
+      },
+    },
+  },
 });
