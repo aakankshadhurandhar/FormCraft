c439ba8e009bddffd942567426695c3d8b39e7e7
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2023-11-24 11:50:39 +0530

    fix:added logout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect } from 'react';
 import './App.css';
 import { useAppDispatch } from '../redux/config/store';
 import { login } from '../redux/slices/authSlice';
