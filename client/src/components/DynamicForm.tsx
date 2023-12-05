dc3eed686a9dbf8b01da0d27ca7cf288910960ae
akhand <akhand3108patel@gmail.com>
2023-12-05 18:48:04 +0530

    MODIFY: Render Form with formschema

--- client/src/components/DynamicForm.tsx
+++ client/src/components/DynamicForm.tsx
@@ -1,67 +1,360 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
-import FormInput from './FormInput';
 import axios from 'axios';
-import * as Yup from 'yup';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Checkbox } from '@/components/ui/checkbox';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 
-const DynamicForm: React.FC<{ formSchema: any }> = ({ formSchema }) => {
-  const initialValues: { [key: string]: string } = {};
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Control, FieldValues, useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-  formSchema.inputs.forEach((input) => {
-    if (input.type === 'none') {
-      return;
+type Option = {
+  label: string;
+  value: string;
+};
+
+type Input = {
+  min?: number | string;
+  max?: number | string;
+  label: string;
+  required: boolean;
+  description?: string;
+  type: string;
+  options?: Option[];
+};
+
+type FormSchema = {
+  owner: string;
+  sharedWith: string[];
+  title: string;
+  description: string;
+  published: boolean;
+  inputs: Input[];
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+const createFormData = (values: { [key: string]: string }) => {
+  const formData = new FormData();
+  for (const [key, value] of Object.entries(values)) {
+    if (Array.isArray(value)) {
+      value.forEach((item) => {
+        formData.append(key, item);
+      });
+    } else if (typeof value === 'object' && value !== null) {
+      const subValues = Object.keys(value);
+      subValues.forEach((subValue) => {
+        formData.append(key, subValue);
+      });
+    } else {
+      formData.append(key, value);
     }
-    initialValues[input.label] = '';
+  }
+  return formData;
+};
+
+const createZodValidationSchema = (formSchema: any) => {
+  const schema: { [key: string]: any } = {};
+
+  formSchema.inputs.forEach((input: any) => {
+    let validator;
+
+    switch (input.type) {
+      case 'small':
+      case 'long':
+      case 'email':
+        validator = z.string();
+        if (input.min && input.max) {
+          validator = validator
+            .min(input.min, `Must be at least ${input.min} characters`)
+            .max(input.max, `Must be at most ${input.max} characters`);
+        }
+        if (input.type === 'email') {
+          validator = validator.email('Must be a valid email address');
+        }
+        break;
+
+      case 'date':
+        //date
+        validator = z
+          .string()
+          .refine(
+            (val) => {
+              return /^\d{4}-\d{2}-\d{2}$/.test(val);
+            },
+            { message: 'Must be a valid date' },
+          )
+          .refine(
+            (val) => {
+              if (input.min) {
+                const minDate = new Date(input.min);
+                const date = new Date(val);
+                return date >= minDate;
+              }
+              return true;
+            },
+            { message: `Must be after ${input.min}` },
+          )
+          .refine(
+            (val) => {
+              if (input.max) {
+                const maxDate = new Date(input.max);
+                const date = new Date(val);
+                return date <= maxDate;
+              }
+              return true;
+            },
+            { message: `Must be before ${input.max}` },
+          );
+
+        break;
+
+      case 'time':
+        validator = z
+          .string()
+          .regex(
+            /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/,
+            'Must be a valid time format HH:mm',
+          )
+          .refine(
+            (val) => {
+              const [hours, minutes] = val.split(':');
+              const [minHours, minMinutes] = input.min.split(':');
+              const totalMinutes = Number(hours) * 60 + Number(minutes);
+              const minTotalMinutes =
+                Number(minHours) * 60 + Number(minMinutes);
+              return totalMinutes >= minTotalMinutes;
+            },
+            {
+              message: `Must be atleast ${input.min} `,
+            },
+          )
+          .refine(
+            (val) => {
+              const [hours, minutes] = val.split(':');
+              const [maxHours, maxMinutes] = input.max.split(':');
+              const totalMinutes = Number(hours) * 60 + Number(minutes);
+              const maxTotalMinutes =
+                Number(maxHours) * 60 + Number(maxMinutes);
+              return totalMinutes <= maxTotalMinutes;
+            },
+            {
+              message: `Must be atmost ${input.max} `,
+            },
+          );
+
+        break;
+
+      case 'multi':
+        validator = z
+          .array(z.string())
+          .refine((value) => value.some((item) => item), {
+            message: 'You have to select at least one option.',
+          });
+        break;
+
+      case 'number':
+        validator = z
+          .string()
+          .refine((val) => !isNaN(Number(val)), {
+            message: 'Must be a number',
+          })
+          .transform((val) => Number(val))
+          .refine((val) => val >= input.min, {
+            message: `Must be at least ${input.min}`,
+          })
+          .refine((val) => val <= input.max, {
+            message: `Must be at most ${input.max}`,
+          });
+
+        break;
+
+      case 'none':
+        return;
+
+      default:
+        validator = z.string();
+    }
+
+    if (input.required) {
+      // validator = validator.nonempty('Required');
+    }
+
+    schema[input.label] = validator;
   });
 
+  return z.object(schema);
+};
 
+const CustomFormField = ({ control, input } : { control:  Control<FieldValues>, input: Input }) => {
+  if (input.type === 'multi') {
+    return (
+      <FormField
+        control={control}
+        name={input.label}
+        render={() => (
+          <FormItem>
+            <FormLabel>{input.label}</FormLabel>
+            <FormDescription>{input.description}</FormDescription>
+            {input.options?.map((option) => (
+              <FormField
+                key={option.value}
+                control={control}
+                name={input.label}
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                    <FormControl>
+                    <Checkbox
+                        checked={field.value?.includes(option.value)}
+                        onCheckedChange={(checked) => {
+                          console.log(checked,field);
+                          return checked
+                            ? field.onChange([...field.value, option.value])
+                            : field.onChange(
+                                field.value?.filter(
+                                  (value: string) => value !== option.value,
+                                ),
+                              );
+                        }}
+                      />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      {option.label}
+                    </FormLabel>
+                  </FormItem>
+                  
+                )}
+              />
+            ))}
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    );
+  }
 
-  const createFormData = (values: { [key: string]: string }) => {
-    const formData = new FormData();
-    for (const [key, value] of Object.entries(values)) {
-      if (Array.isArray(value)) {
-        value.forEach((item) => {
-          formData.append(key, item);
-        });
-      } else if (typeof value === 'object' && value !== null) {
-        const subValues = Object.keys(value);
-        subValues.forEach((subValue) => {
-          formData.append(key, subValue);
-        });
-      } else {
-        formData.append(key, value);
-      }
+  return (
+    <FormField
+      control={control}
+      key={input.label}
+      name={input.label}
+      render={({ field }) => {
+        let inputComponent;
+        switch (input.type) {
+          case 'none':
+            inputComponent = null;
+            break;
+          case 'number':
+            inputComponent = <Input {...field} type="number" />;
+            break;
+          case 'long':
+            inputComponent = <Textarea {...field} />;
+            break;
+          case 'email':
+            inputComponent = <Input {...field} type="email" />;
+            break;
+          case 'date':
+            inputComponent = <Input {...field} type="date" />;
+            break;
+          case 'time':
+            inputComponent = <Input {...field} type="time" />;
+            break;
+          // case 'multi':
+
+          case 'radio':
+            inputComponent = (
+              <RadioGroup
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+                className="flex flex-col space-y-1"
+              >
+                <FormDescription>{input.description}</FormDescription>
+                {input?.options?.map((option: Option) => (
+                  <FormItem
+                    className="flex items-center space-x-3 space-y-0"
+                    key={option.value}
+                  >
+                    <FormControl>
+                      <RadioGroupItem value={option.value} />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      {option.label}
+                    </FormLabel>
+                  </FormItem>
+                ))}
+              </RadioGroup>
+            );
+            break;
+
+          default:
+            inputComponent = <Input {...field} />;
+        }
+        return (
+          
+            <FormItem>
+            <FormLabel>{input.label}</FormLabel>
+            <FormDescription>{input.description}</FormDescription>
+            <FormControl>{inputComponent}</FormControl>
+            <FormMessage />
+          </FormItem>
+        );
+      }}
+    />
+  );
+};
+
+const DynamicForm = ({ formSchema } : { formSchema: FormSchema}) => {
+  const validationSchema = createZodValidationSchema(formSchema);
+  const defaultValues: { [key: string]: any } = {};
+
+  formSchema.inputs.forEach((input: any) => {
+    if (input.type === 'multi') {
+      defaultValues[input.label] = [];
     }
-    return formData;
-  };
-
-  const onSubmit = async (values: { [key: string]: string }, actions: any) => {
-    const formData = createFormData(values);
-    console.log('Form data:', values);
-  
-    try {
-      const response = await axios.post(`/api/forms/${formSchema._id}/responses`, formData);
-      console.log('Server response:', response.data);
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  
-    actions.setSubmitting(false);
+  });
+  const form = useForm({
+    resolver: zodResolver(validationSchema),
+    mode: 'onChange',
+    defaultValues
+  });
+  const onSubmit = async (data: any) => {
+    console.log('Data', data);
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({ isSubmitting }) => (
-        <Form style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-          {formSchema.inputs.map((input) => (
-            <FormInput key={input.label} input={input} />
-          ))}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="container mx-auto px-4 py-8 w-1/2  bg-teal-100">
+        <h1 className="text-2xl font-bold">{formSchema.title}</h1>
+        <p className="text-gray-600">{formSchema.description}</p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            {formSchema.inputs.map((input: any) => (
+              <CustomFormField
+                key={input.label}
+                control={form.control}
+                input={input}
+              />
+            ))}
+
+            <Button type="submit">Submit</Button>
+          </form>
         </Form>
-      )}
-    </Formik>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,116 +1,127 @@
 import React from 'react';
-import DynamicForm from '../components/DynamicForm';
+import DynamicForm from '@/components/DynamicForm';
 
 interface LoginProps {
   // Define the props for the component here
 }
 
 const formSchema = {
-        "owner": "6565e24b66bdad35df7a3004",
-        "sharedWith": [],
-        "title": "SuperForm",
-        "description": "Includes all types of inputs",
-        "published": true,
-        "inputs": [
-            {
-                "min": 0,
-                "max": 100,
-                "label": "Full Name",
-                "required": false,
-                "description": "Enter your full name",
-                "type": "small"
-            },
-            {
-                "min": 0,
-                "max": 100,
-                "label": "Email Address",
-                "required": false,
-                "type": "email"
-            },
-            {
-                "min": 0,
-                "max": 100,
-                "label": "Feedback",
-                "required": false,
-                "description": "Enter your feedback",
-                "type": "long"
-            },
-            {
-                "options": [
-                    {
-                        "label": "Machine Learning",
-                        "value": "ML"
-                    },
-                    {
-                        "label": "Backend",
-                        "value": "BACKEND"
-                    },
-                    {
-                        "label": "Frontend",
-                        "value": "FRONTEND"
-                    },
-                    {
-                        "label": "Android",
-                        "value": "ANDROID"
-                    }
-                ],
-                "label": "Preference",
-                "required": false,
-                "type": "multi"
-            },
-            {
-                "options": [
-                    {
-                        "label": "Male",
-                        "value": "M"
-                    },
-                    {
-                        "label": "Female",
-                        "value": "F"
-                    },
-                    {
-                        "label": "Prefer not to disclose",
-                        "value": "NA"
-                    }
-                ],
-                "label": "Gender",
-                "required": false,
-                "type": "radio"
-            },
-            {
-                "label": "Date picker",
-                "required": false,
-                "type": "date"
-            },
-            {
-                "label": "Time Picker",
-                "required": false,
-                "type": "time"
-            },
-            {
-                "label": "No Input Label",
-                "required": false,
-                "type": "none"
-            }
-        ],
-        "_id": "656661c8b6f191aca5ac8e67",
-        "createdAt": "2023-11-28T21:55:20.408Z",
-        "updatedAt": "2023-11-28T21:55:20.408Z",
-        "__v": 0
-    }
-
-
+  owner: '6565e24b66bdad35df7a3004',
+  sharedWith: [],
+  title: 'SuperForm',
+  description: 'Includes all types of inputs',
+  published: true,
+  inputs: [
+    {
+      min: 3,
+      max: 100,
+      label: 'Full Name',
+      required: true,
+      description: 'Enter your full name',
+      type: 'small',
+    },
+    {
+      min: 18,
+      max: 100,
+      label: 'Age',
+      required: true,
+      description: 'Enter your Age',
+      type: 'number',
+    },
+    {
+      min: 0,
+      max: 100,
+      label: 'Email Address',
+      required: false,
+      type: 'email',
+    },
+    {
+      min: 10,
+      max: 100,
+      label: 'About_me',
+      required: true,
+      description: 'Tell us about yourself',
+      type: 'long',
+    },
+    {
+      options: [
+        {
+          label: 'Machine Learning',
+          value: 'ML',
+        },
+        {
+          label: 'Backend',
+          value: 'BACKEND',
+        },
+        {
+          label: 'Frontend',
+          value: 'FRONTEND',
+        },
+        {
+          label: 'Android',
+          value: 'ANDROID',
+        },
+      ],
+      label: 'Preference',
+      description: 'Select your preference',
+      required: true,
+      type: 'multi',
+    },
+    {
+      options: [
+        {
+          label: 'Male',
+          value: 'M',
+        },
+        {
+          label: 'Female',
+          value: 'F',
+        },
+        {
+          label: 'Prefer not to disclose',
+          value: 'NA',
+        },
+      ],
+      label: 'Gender',
+      required: false,
+      type: 'radio',
+    },
+    {
+      min: '2000-07-12',
+      max: '2030-01-01',
+      label: 'Date picker',
+      required: false,
+      type: 'date',
+    },
+    {
+      min: '09:30',
+      max: '20:00',
+      label: 'Time Picker',
+      required: false,
+      type: 'time',
+    },
+    {
+      label: 'No Input Label',
+      description: 'This is a sample description', 
+      required: false,
+      type: 'none',
+    },
+  ],
+  _id: '6569be5d69ad31fed404741d',
+  createdAt: '2023-12-01T11:07:09.392Z',
+  updatedAt: '2023-12-01T11:07:09.392Z',
+  __v: 0,
+};
 
 const Login: React.FC<LoginProps> = () => {
   // Implement the component logic here
-
+  console.log("Login");
   return (
     <div>
       <h1>login</h1>
-      <DynamicForm formSchema={formSchema} />
+      <DynamicForm formSchema={formSchema}></DynamicForm>
     </div>
   );
 };
 
 export default Login;
-
